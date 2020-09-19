62188407419c583560c7cfeddb15091e530c454e
shrehanchatterjee <70361874+shrehanchatterjee@users.noreply.github.com>
2020-09-18 20:50:49 -0700

    Create mycalender1.py
    This code asks a question by using "user input." It has the schedules for Monday and Tuesday, when user states what day he would like and presses enter it shows the schedule. I used if/else statement for this. I think you need python 2.7 at least for this.

---
+++ mycalender1.py
@@ -0,0 +1,8 @@
+day=input("Which day would you like the schedule for?")
+print("Schedule for", day)
+if day == "Monday":
+  print("Day     Time     Subject                                   Monday 9:10 - 10:15  P.E.                                       10:35 - 11:40 Exploratory Wheel                                12:10 - 1:15 Science")
+elif day == "Tuesday":
+ print("Day     Time     Subject                                    Tuesday 9:10 - 10:15 Math                                    10:35 - 11:40 Language Arts                                12:10 - 1:15 Social Studies")
+else:
+ print("We only have the schedule for Monday and Tuesday.")
